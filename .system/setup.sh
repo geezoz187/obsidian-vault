613b645d00f12b8383991a7df922d95344cd876d
geezoz187 <git config --global user.name>
2024-09-22 22:47:44 +0200

    Initial commit

--- .system/setup.sh
+++ .system/setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 rootPath=$(git rev-parse --show-toplevel)
-ln -s $rootPath/.system/post-commit $rootPath/.git/hooks/post-commit
-ln -s $rootPath/.system/pre-commit $rootPath/.git/hooks/pre-commit
+
+# Check if post-commit file exists
+if [ -e "$rootPath/.git/hooks/post-commit" ]; then
+  # Delete the existing post-commit file
+  rm "$rootPath/.git/hooks/post-commit"
+fi
+
+# Check if pre-commit file exists
+if [ -e "$rootPath/.git/hooks/pre-commit" ]; then
+  # Delete the existing pre-commit file
+  rm "$rootPath/.git/hooks/pre-commit"
+fi
+
+# Create symlinks for post-commit and pre-commit
+ln -s "$rootPath/.system/post-commit" "$rootPath/.git/hooks/post-commit"
+ln -s "$rootPath/.system/pre-commit" "$rootPath/.git/hooks/pre-commit"
